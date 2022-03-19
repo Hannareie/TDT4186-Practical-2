2eb5b04b13e091b5161b4eb17a1d4b7782ca1438
Hanna <hannahreiestad@gmail.com>
2022-03-19 15:43:05 +0100

    Working single threaded server

--- mtwwwd.c
+++ mtwwwd.c
@@ -1,156 +1,120 @@
-/*
-    Commands to run a server
-    -> gcc -pthread -o server mtwwwd.c (server kan byttes til annet navn)
-    -> ./server
-    This generates an execute file named server. Remember to delete this file before pushing new code.
-    
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <pthread.h>
-#include <semaphore.h>
-//#include "sem.h"
-#include "bbuffer.h"
+#include <sys/sendfile.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-#define SERVERPORT 8989
-#define BUFSIZE 4096
-#define SERVER_BACKLOG 100
+#define MAXREQ (4096*1024)
 
-typedef struct sockaddr_in SA_IN;
-typedef struct sockaddr SA;
-//typedef BNDBUF buffer;
+char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ];
+void error(const char *msg) {
+    perror(msg); 
+    exit(1);
+}
 
+int main(int argc, char *argv[]) {
+    char www_path[MAXREQ];
+    char www_path_head[1024];
+    int port;
+    int num_threads;
+    int num_buffer_slots;
+    FILE *file;
+    char *file_data;
+    int file_size;
 
-void * handle_connection(void* client_socket);
-int check(int exp, const char *msg);
-//void * thread_function(void *arg);
+    //Vil kjøre programmet på formatet: mtwwwd www-path port #threads #bufferslots
+    //www-path er (for meg) /home/hannareie/Oving_2_OPSYS/ 
+    //threads og bufferslots kan velges til random nummer
+    
+    if (argc > 1) {
+        strcpy(www_path_head, argv[1]);
+    } else {
+        error("No www-path is given\n");
+    } 
+    if (argc > 2) {
+        sscanf(argv[2], "%d", &port);
+    } else {
+        error("No port is given\n");
+    }
+    if (argc > 3) {
+        sscanf(argv[3], "%d", &num_threads);
+    } else {
+        error("Number of threads is not given\n");
+    }
+    if (argc > 4) {
+        sscanf(argv[4], "%d", &num_buffer_slots);
+    } else {
+        error("Number of buffer slots is not given\n");
+    }
+    
+    printf("Program is started at port %i with given www_path: %s \n", port, www_path_head);
+    
+    int server_socket, client_socket, data;
+    socklen_t client_len;
+    struct sockaddr_in server_address, client_address;
+    
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        error("Error opening socket\n");
+    }
+    printf("Socket successfull created\n");
+    
+    bzero((char *) &server_address, sizeof(server_address));
 
-sem_t mutex;
-
-int main(int argc, char **argv)
-{
-    int server_socket, client_socket, addr_size;
-    SA_IN server_addr, client_addr;
-    //buffer buff = bb_init(BUFSIZE);
-
-    check((server_socket = socket(AF_INET , SOCK_STREAM , 0)), 
-            "Failed to create socket");
-
-    //Initialize the address struct
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(SERVERPORT);
-
-    check(bind(server_socket, (SA*)&server_addr, sizeof(server_addr)), 
-            "Bind failed");
-    check(listen(server_socket, SERVER_BACKLOG), 
-            "Listen failed");
-    printf("Listening...");
-
-    while (true) {
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(port);
+    
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+        error("Error while binding socket\n");
+    }
+    
+    if (listen(server_socket, 10) != 0) {
+        error("Error occured while listening to socket \n");
+    }
+    printf("Listening...\n");
+    
+    while(1) {
         printf("Waiting for connections... \n");
-        //Wait for and accept an incoming connection
-        addr_size = sizeof(SA_IN);
-        check(client_socket = 
-                accept(server_socket, (SA*)&client_addr, (socklen_t*)&addr_size), 
-                "Accect failed");
-        printf("Connected\n");
+        client_len = sizeof(client_address);
+        client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_len);
+        
+        if (client_socket < 0) {
+            error("Accept failed\n");
+        } 
+        printf("Connection accepted \n");
 
-        /*
-        SEM mutex = sem_init(1);
-        pthread_t t;
-        pthread_mutex_lock(&mutex);
-        bb_add(buff, pclient);
-        pthread_mutex_unlock(&mutex);
-        */
+        bzero(buffer, sizeof(buffer));
 
-        sem_init(&mutex, 0, 1);
-        pthread_t t;
-        int *pclient = malloc(sizeof(int));
-        *pclient = client_socket;
-
-        if (pthread_create(&t, NULL, handle_connection, &client_socket) != 0)
-            // Error in creating thread
-            printf("Failed to create thread\n");
-        sem_destroy(&mutex);
-    }
-    return 0;
-}
-
-int check(int exp, const char *msg) {
-    if (exp < 0) {
-        perror(msg);
-        exit(1);
-    }
-    return exp;
-}
-
-/*
-void * thread_function(void *arg) {
-    while (true){
-        int *pclient;
-        pthread_mutex_lock(&mutex);
-        pclient = bb_get(&buff);
-        pthread_mutex_unlock(&mutex);
-        if (pclient != NULL) {
-            handle_connection(pclient);
+        data = read(client_socket, buffer, sizeof(buffer) - 1); 
+        if (data < 0) error("Reading from socket failed");
+        
+        strcpy(www_path, www_path_head);
+        strtok(buffer, " ");
+        strcat(www_path, strtok(NULL, " "));
+        
+        file = fopen(www_path,"r");
+        
+        if (file != NULL) {
+            size_t new_len = fread(body, sizeof(char), MAXREQ, file);
+            if (ferror(file) != 0 ) {
+                fputs("Reading from socket failed", stderr);
+            } else {
+                body[new_len++] = '\0'; 
+            }
+            fclose(file);
         }
-    }
-}
-*/
-
-void * handle_connection(void* p_client_socket) {
-
-    sem_wait(&mutex);
-    printf("\nEntered..\n");
-
-    int client_socket = *((int*)p_client_socket);
-    char buffer[BUFSIZE];
-    size_t bytes_read;
-    int msgsize = 0;
-    char actualpath[PATH_MAX+1];
-
-    while((bytes_read = read(client_socket, buffer+msgsize, sizeof(buffer)-msgsize-1))> 0) {
-        msgsize += bytes_read;
-        if (msgsize > BUFSIZE-1 || buffer[msgsize-1] == '\n') break;
-    }
-
-    check(bytes_read, "revc error");
-    buffer[msgsize-1] = 0;
-
-    printf("REQUEST: %s\n", buffer);
-    fflush(stdout);
-
-    if (realpath(buffer, actualpath) == NULL) {
-        printf("Error(Bad path): %s\n", buffer);
+        
+        snprintf(msg, sizeof(msg), 
+            "HTTP/1.0 200 OK\n"
+            "Content-Type: text/html\n"
+            "Content-Length: %d\n\n%s",
+            strlen(body), body);
+        
+        data = write(client_socket, msg, strlen(msg));
+        if (data < 0) error("Error writing to socket");
         close(client_socket);
-        return NULL;
     }
-
-    FILE *fp = fopen(actualpath, "r");
-    if (fp == NULL) {
-        printf("ERROR(open): %s\n", buffer);
-        close(client_socket);
-        return NULL;
-    }
-
-    while((bytes_read = fread(buffer, 1, BUFSIZE, fp)) > 0) {
-        printf("sending %zu bytes\n", bytes_read);
-        write(client_socket, buffer, bytes_read);
-    }
-
-    printf("\nJust Exiting...\n");
-    sem_post(&mutex);
-
-    close(client_socket);
-    fclose(fp);
-    printf("Closing connection\n");
-    //pthread_exit(NULL);
-    return NULL;
 }
